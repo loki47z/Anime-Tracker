eb274f5ce4d882019cc662f40d494128e25c5261
Thomas Manong'a <thomasmanonga7@gmail.com>
2025-05-29 22:23:27 +0300

    Search Usage added

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const axios = require('axios')
 
 router.get('/', (req,res)=>{
     res.render('index', {user: 'Anime tracker', title: "Home"})
+})
+
+router.get('/search', async (req, res)=>{
+    const query = req.query.q
+    let results = []
+    if(query) {
+        try{
+            const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}`)
+            results = response.data.data
+        } catch(error){
+            console.error('Error catching anime: ', error)
+        }
+    }
+    res.render('search', {title: 'Search Anime', user: 'Thomas', results, query})
 })
 
 module.exports = router
